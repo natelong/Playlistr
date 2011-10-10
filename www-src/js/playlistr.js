ce0f0bd329a72ad30752b6bd9ed9ab8f1f626ca7
Nate Long <long.nathaniel@gmail.com>
2011-10-09 21:23:45 -0700

    got the script to a point where it actually plays a little music. now we just need a ui

--- www-src/js/playlistr.js
+++ www-src/js/playlistr.js
@@ -1,6 +1,7 @@
 var playlistr = (function(){
-//	var apiUrl = 'http://api.rdio.com/1/';
 	var apiUrl = '/api/';
+	var playerId = 'player';
+	var player;
 
 	/**
 	 * Send a request to the API
@@ -10,7 +11,7 @@ var playlistr = (function(){
 	 * @param {function(string)} onSuccess A callback to be called when the response is received
 	 * @param {function(string)} onFail A callback to be called if the request fails
 	 * @param {Object=} scope An object to be set as the scope of the success or fail callbacks (optional)
-	 **/
+	 */
 	var get = function get( service, args, onSuccess, onFail, scope ){
 		var i, count;
 		var successReg = /2\d{2}/;
@@ -27,7 +28,7 @@ var playlistr = (function(){
 				if( successReg.test( req.status ) ){
 					onSuccess.call( scope, req.responseText );
 				}else{
-					onFail.call( scope, req.responseText )
+					onFail.call( scope, req.responseText );
 				}
 			}
 		};
@@ -45,7 +46,67 @@ var playlistr = (function(){
 		req.send( requestText );
 	};
 
+	/**
+	 * Helper function to output the result of an ajax call to the console.
+	 */
+	var outputToConsole = function outputToConsole( data ){
+		try{
+			console.log( JSON.parse( data ) );
+		}catch( e ){
+			console.log( data );
+		}
+	};
+
 	return{
+		/**
+		 * initialize the playlistr app
+		 */
+		init: function init(){
+			var scope = this;
+			var embedPlayer = function embedPlayer( token ){
+				var flashvars = {
+					playbackToken: token,
+					domain: document.domain,
+					listener: 'playlistr'
+				};
+				var params = {
+					allowScriptAccess: 'always'
+				};
+				var attributes = {};
+				swfobject.embedSWF(
+					'http://www.rdio.com/api/swf/',
+					playerId,
+					1,
+					1,
+					'9.0.0',
+					'expressInstall.swf',
+					flashvars,
+					params,
+					attributes
+				);
+			};
+			var onTokenReceived = function( data ){
+				var token;
+				try{
+					token = JSON.parse( data ).result;
+				}catch( e ){
+					throw "Couldn't parse playback token.";
+				}
+				embedPlayer( token );
+			};
+			this.getPlaybackToken( onTokenReceived );
+		},
+		getPlayer: function getPlayer(){
+			return player;
+		},
+		ready: function ready(){
+			player = document.getElementById( playerId );
+			player.rdio_play( 't7342831' );
+		},
+		playStateChanged: function playStateChanged( playState ) {
+			var states = [ 'paused', 'playing', 'stopped', 'buffering', 'paused' ];
+			console.log( states[ playState ] );
+		},
 		/**
 		 * Get the list of albums for the given artist
 		 *
@@ -56,14 +117,19 @@ var playlistr = (function(){
 			var args = {
 				query: artistName,
 				types: 'artist'
-			}
-			var onSuccess = function onSuccess( data ){
-				console.log( JSON.parse( data ) );
-			}
-			var onFail = function onFail( data ){
-				console.log( data );
-			}
-			get( 'search', args, onSuccess, onFail );
+			};
+			get( 'search', args, outputToConsole, outputToConsole );
+		},
+		getPlaybackToken: function getPlaybackToken( onComplete, onFail ){
+			onComplete = onComplete || outputToConsole;
+			onFail = onFail || outputToConsole;
+			var scope = this;
+			var args = {
+				domain: document.domain
+			};
+			get( 'getPlaybackToken', args, onComplete, onFail );
 		}
-	}
+	};
 }());
+
+playlistr.init();
