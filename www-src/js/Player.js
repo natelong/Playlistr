fb6aca751414c8f284334f992278a38f0201f199
Nate Long <long.nathaniel@gmail.com>
2011-10-23 22:13:55 -0700

    Added a bunch of stuff to the player script, since it's basically the interface between the browser and RDIO's playback functions

--- www-src/js/Player.js
+++ www-src/js/Player.js
@@ -1,7 +1,19 @@
 var Player = function( playerId ){
 	this.playerId = playerId;
-	this.playerElement = null;
 };
+
+Player.prototype.playerElement = null;
+Player.prototype.statusElement = null;
+Player.prototype.currentState = null;
+Player.prototype.currentTrack = null;
+
+Player.prototype.states = [
+	'Paused',
+	'Playing',
+	'Stopped',
+	'Buffering',
+	'Paused'
+];
 
 /**
  * Embeds the flash player into the page
@@ -12,7 +24,7 @@ Player.prototype.embedPlayer = function embedPlayer( token ){
 	var flashvars = {
 		playbackToken: token,
 		domain: document.domain,
-		listener: 'playlistr.modules.player.callbacks'
+		listener: 'playlistr.player'
 	};
 	var params = {
 		allowScriptAccess: 'always'
@@ -29,8 +41,19 @@ Player.prototype.embedPlayer = function embedPlayer( token ){
 		params,
 		attributes
 	);
-	this.playerElement = document.getElementById( this.playerId );
+	this.playerElement = document.querySelector( '#' + this.playerId );
+	this.statusElement = document.querySelector( '#playerStatus' );
 };
+
+/**
+ * Update the status listing to reflect the current state and playing track
+ */
+Player.prototype.updateStatus = function updateStatus(){
+	this.statusElement.innerHTML = playlistr.templates.trackInfo({
+		track: this.currentTrack,
+		status: this.states[ this.currentState ]
+	});
+}
 
 /**
  * Immediately play content from the selected source.
@@ -47,20 +70,40 @@ Player.prototype.pause = function pause(){
 };
 
 /**
- * Handle events created by the player
+ * A function to call when the player has been initialized completely
  */
-Player.prototype.callbacks = {
-	/**
-	 * A function to call when the player has been initialized completely
-	 */
-	ready: function ready(){
-		console.log( 'Player Ready' );
-	},
-	/**
-	 * When the player state changes, respond to 
-	 */
-	playStateChanged: function playStateChanged( playState ) {
-		var currentState = states[ playState ];
-		console.log( currentState );
+Player.prototype.ready = function ready(){
+	console.log( 'Player Ready' );
+	this.statusElement.innerHTML = 'Ready when you are :)';
+};
+
+/**
+ * When the player state changes, respond to 
+ */
+Player.prototype.playStateChanged = function playStateChanged( playState ) {
+	this.currentState = playState;
+
+	this.updateStatus();
+
+	switch( playState ){
+		case 0:
+		break;
+		case 1:
+		break;
+		case 2: //stopped
+		break;
+		case 3:
+		break;
+		case 4:
+		break;
 	}
 };
+
+/**
+ * When the current track changes
+ */
+Player.prototype.playingTrackChanged = function playingTrackChanged( playingTrack, sourcePosition ){
+	if( playingTrack ){
+		this.currentTrack = playingTrack;
+	}
+}
